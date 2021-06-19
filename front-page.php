b767aff6bc1147c56fd163445496a753c51bf805
aayushali <aayushkhan51@gmail.com>
2021-06-19 22:25:47 +0545

    Slider Completed

--- front-page.php
+++ front-page.php
@@ -1,46 +1,37 @@
 <?php
-
 get_header();
-
-
 ?>
-
-
     <!-- Start home section -->
     <div id="home">
         <!-- Start cSlider -->
         <div id="da-slider" class="da-slider">
             <div class="triangle"></div>
-            <!-- mask elemet use for masking background image -->
+            <!-- mask element use for masking background image -->
             <div class="mask"></div>
             <!-- All slides centred in container element -->
             <div class="container">
-                <!-- Start first slide -->
-                <?php
-                    $args = array(
-                            'post_type' => 'slider',
-                    );
-                    $the_query = new WP_Query($args);
-                    if($the_query->have_posts()):
-                        while($the_query->have_posts()):$the_query->the_post();
-                ?>
-
-                <div class="da-slide">
-                    <h2 class="fittext2"><?php the_title(); ?></h2>
-                    <h4><?php echo get_the_excerpt() ?></h4>
-                    <?php the_content(); ?>
-                    <a href="#" class="da-link button">Read more</a>
-                    <div class="da-img">
-                        <img src="<?php the_post_thumbnail_url(); ?>" alt="image01" width="320">
-                    </div>
-                </div>
-
-                        <?php
-                        endwhile;
-                        endif;
-                        wp_reset_postdata();
-                        ?>
-                <!-- Start third slide -->
+				<?php
+				$args      = array(
+					'post_type' => 'slider',
+				);
+				$the_query = new WP_Query( $args );
+				if ( $the_query->have_posts() ):
+					while ( $the_query->have_posts() ):$the_query->the_post();
+						?>
+                        <div class="da-slide">
+                            <h2 class="fittext2"><?php the_title(); ?></h2>
+                            <h4><?php echo get_the_excerpt() ?></h4>
+							<?php the_content(); ?>
+                            <a href="#" class="da-link button">Read more</a>
+                            <div class="da-img">
+                                <img src="<?php the_post_thumbnail_url(); ?>" alt="image01" width="320">
+                            </div>
+                        </div>
+					<?php
+					endwhile;
+				endif;
+				wp_reset_postdata();
+				?>
                 <!-- Start cSlide navigation arrows -->
                 <div class="da-arrows">
                     <span class="da-arrows-prev"></span>
@@ -51,8 +42,4 @@ get_header();
         </div>
     </div>
     <!-- End home section -->
-
-
-
-
 <?php get_footer(); ?>
